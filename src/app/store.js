871966144eebcf0699ba7fb75b5807de69e3f5cf
Vinayak <myselfvinayakhegde@gmail.com>
2023-03-16 03:05:55 +0530

    build done

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import listsReducer from "../features/listsSlice"
-
-export const store = configureStore({
-  reducer: {
-    lists: listsReducer
-  },
-});
