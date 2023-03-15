8d019d34ea56ea9e30e7b85e686ae668fe40b099
Vinayak <myselfvinayakhegde@gmail.com>
2023-03-16 03:14:14 +0530

    refactor : moved assets to public

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,8 @@ import "./Header.css";
 function Header() {
   return (
     <div className="header-container">
-      <img id="logo" src="/src/assets/Trello-logo.png"></img>
-      <img id="menu-bar" src="/src/assets/burger-menu.svg"></img>
+      <img id="logo" src="/public/assets/Trello-logo.png"></img>
+      <img id="menu-bar" src="/public/assets/burger-menu.svg"></img>
     </div>
   );
 }
